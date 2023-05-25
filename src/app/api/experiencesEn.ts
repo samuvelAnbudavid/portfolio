9f67807805fe5e2db3d702e7e045618ac36190db
samuvel anbudavid <sam.a.bca@gmail.com>
2023-05-26 01:47:07 +0530

    initial setup

---
+++ src/app/api/experiencesEn.ts
@@ -0,0 +1,39 @@
+export const experiencesEn = [
+    {
+        title: "Senior Engineer js",
+        company: 'QBurst',
+        period: "2022 - still",
+        technologies: 'Node js , Express js, MYSQL,moongoDB,Redis,Typescript,Postgrel',
+        missions: [
+            "Development of a  maintenance applications",
+           
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Backend Engineer Node js",
+        company: 'doodleblue Innovations, Chennai, India',
+        period: "2021 - 2022 ",
+        technologies: 'Node js , Express js, MYSQL,moongoDB,Redis',
+        missions: [
+            "Development of a stock maintenance application by warehouse(Ripplr).",
+            "Users and corrective maintenance of MZAALO applications.",
+        ],
+        detailIsDisplayed: false,
+    },
+    {
+        title: "Project Engineer",
+        company: 'Kasponteach works,Chennai,India',
+        period: "2019 - 2021 ",
+        technologies: 'HTML,CSS,Javascript,PHP,Angular,Laravel',
+        missions: [
+            "Implementation of a theme manager (colors, logo).",
+            "Setting up a virtual folder manager that can contain Heart Beat web apps.",
+            "Development of a resource saving interface.",
+            "Creating customizable web pages:Heart Beat .",
+        ],
+        detailIsDisplayed: false,
+    },
+   
+];
+
